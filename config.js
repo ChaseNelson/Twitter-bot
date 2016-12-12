8bdc03522c0a0e4c7baf11c60017829ffd2b775a
Chase Nelson <Chase.a.nelson@gmail.com>
2016-12-12 13:37:48 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,6 @@
 module.exports = {
-  consumer_key:         'vbZuPsLLjZphbwRsrIe54REgb',
-  consumer_secret:      '56naiDPZ2iP2iatIf0v9n5j7zLX79hDXXPTQ944hykHCljTwS1',
-  access_token:         '808347769534554112-hkv1ymF35EDCrAGagqPS2WdrzEiDaXl',
-  access_token_secret:  'yIhPJYyGaqMTy71l3HSJRkI0gbVpZVcIfoY35x1XeW0Iu',
-
+  consumer_key:         '..',
+  consumer_secret:      '..',
+  access_token:         '..',
+  access_token_secret:  '..',
 }
